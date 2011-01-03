0587e9beedaa76a6d9c66a5a2308ea491d968af4
Rob Eastwood <rob.eastwood8@gmail.com>
2011-01-04 08:06:27 +1000

    added conky startup script

---
+++ conky/ConkyStartup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# click to start, click to stop
+
+if pidof conky | grep [0-9] > /dev/null
+then
+exec killall conky
+else
+
+# run scripts
+# ruby ~/scripts/cronjobs/internodeData_v11.rb # eg
+
+# sleep 30  # sleep not required for xfce on startup - 30 or more for others
+conky -c ~/.config/conky/conky_topleft &
+
+exit
+fi
