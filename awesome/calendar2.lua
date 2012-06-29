0751b23fa4554474b6188b14972855a58219af46
Rob Eastwood <rob.eastwood8@gmail.com>
2012-06-30 09:21:41 +1000

    calendar2.lua - changed font rendering of today
    - from underline to red

--- awesome/calendar2.lua
+++ awesome/calendar2.lua
@@ -17,7 +17,8 @@ local naughty = require("naughty")
 module("calendar2")
 
 local calendar = {}
-local current_day_format = "<u>%s</u>"
+-- local current_day_format = "<u>%s</u>"
+local current_day_format = "<span color='red'>%s</span>"
 
 function displayMonth(month,year,weekStart)
         local t,wkSt=os.time{year=year, month=month+1, day=0},weekStart or 1
