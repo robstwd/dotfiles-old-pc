16c404efae5f5d99ecc8ac42a5451b6e0177739f
Rob Eastwood <rob.eastwood8@gmail.com>
2012-07-12 22:52:24 +1000

    copy_dotfiles.rb - added extra collection
    - added ~/bin files
    - added STDOUT print of numbers of files copied
    - comented out dwm file copy for now as erroring out (needs further
      investid\gation)

--- copy_dotfiles.rb
+++ copy_dotfiles.rb
@@ -11,6 +11,7 @@ openboxfiles << "#{path}menu.xml"
 openboxfiles << "#{path}autostart"
 openboxfiles << "#{path}rc.xml" 
 FileUtils.cp(openboxfiles, "#{dest}openbox/")
+puts "Copied #{openboxfiles.length} openbox files"
 
 # conky config files
 conkyfiles = []
@@ -19,13 +20,16 @@ conkyfiles << "#{path}conky_topleft"
 conkyfiles << "#{path}scripts/ConkyStartup.sh"
 conkyfiles << "#{path}conky_bottomleft"
 conkyfiles << "#{path}conky_topright"
+conkyfiles << "#{path}conky_dwm"
 FileUtils.cp(conkyfiles, "#{dest}conky/")
+puts "Copied #{conkyfiles.length} conky files"
 
 # bash
 bashfiles = []
 path = "/home/rob/"
 bashfiles << "#{path}.bashrc"
 FileUtils.cp(bashfiles, "#{dest}bash/")
+puts "Copied #{bashfiles.length} bash files"
 
 # Arch linux config files
 archfiles = []
@@ -43,6 +47,7 @@ archfiles << "#{path}pacman.conf"
 archfiles << "#{path}pacman.d/mirrorlist"
 archfiles << "/home/rob/.xinitrc"
 FileUtils.cp(archfiles, "#{dest}arch/")
+puts "Copied #{archfiles.length} arch files"
 
 # Git
 gitfiles = []
@@ -50,6 +55,7 @@ path = "/home/rob/"
 gitfiles << "#{path}.gitconfig"
 gitfiles << "/home/rob/.config/git/git-prompt.sh"
 FileUtils.cp(gitfiles, "#{dest}git/")
+puts "Copied #{gitfiles.length} git files"
 
 # Terminal urxvt
 urxvtfiles = []
@@ -57,24 +63,28 @@ path = "/home/rob/"
 urxvtfiles << "#{path}.Xdefaults"
 urxvtfiles << "/home/rob/.config/xfce4/helpers.rc"
 FileUtils.cp(urxvtfiles, "#{dest}terminal/")
+puts "Copied #{urxvtfiles.length} urxvt files"
 
 # tint2
 tint2files = []
 path = "/home/rob/.config/tint2/"
 tint2files << "#{path}tint2rc"
 FileUtils.cp(tint2files, "#{dest}tint2/")
+puts "Copied #{tint2files.length} tint2 files"
 
 # xorg
 xorgfiles = []
 path = "/etc/X11/"
 xorgfiles << "#{path}xorg.conf"
 FileUtils.cp(xorgfiles, "#{dest}xorg/")
+puts "Copied #{xorgfiles.length} xorg files"
 
 # slim
 slimfiles = []
 path = "/etc/"
 slimfiles << "#{path}slim.conf"
 FileUtils.cp(slimfiles, "#{dest}slim/")
+puts "Copied #{slimfiles.length} slim files"
 
 # testing
 testingfiles = []
@@ -82,22 +92,33 @@ path = "/home/rob/"
 testingfiles << "#{path}.autotest"
 testingfiles << "#{path}.rspec"
 FileUtils.cp(testingfiles, "#{dest}testing/")
+puts "Copied #{testingfiles.length} testing files"
 
 # vim files
 vimfiles = Dir.glob("/home/rob/.vim/*/*")
-puts vimfiles.length
 path = "/home/rob/"
 vimfiles << "#{path}.vimrc"
 FileUtils.cp_r(vimfiles, "#{dest}vim/")
+puts "Copied #{vimfiles.length} vim files"
 
 # motion files
 motionfiles = Dir.glob("/home/rob/.motion/*")
 FileUtils.cp_r(motionfiles, "#{dest}motion/")
+puts "Copied #{motionfiles.length} motion files"
 
 # awesome files
 awesomefiles = Dir.glob("/home/rob/.config/awesome/*")
 FileUtils.cp_r(awesomefiles, "#{dest}awesome/")
+puts "Copied #{awesomefiles.length} awesome files"
 
 # dwm files
 dwmfiles = Dir.glob("/home/rob/dwm/*")
-FileUtils.cp_r(dwmfiles, "#{dest}dwm/")
+#dwmfiles << "/usr/bin/dwm­-personalised"
+#dwmfiles << "/usr/share/xsessions/dwm­-personalised.desktop"
+#FileUtils.cp_r(dwmfiles, "#{dest}dwm/")
+#puts "Copied #{dwmfiles.length} dwm files"
+
+# ~/bin files
+binfiles = Dir.glob("/home/rob/bin/*")
+FileUtils.cp_r(binfiles, "#{dest}bin/")
+puts "Copied #{binfiles.length} bin files"
