92ba0ad07a890ce9098f0523b8e04725ace14afb
Rob Eastwood <rob.eastwood8@gmail.com>
2012-07-17 18:44:28 +1000

    updating dwm collection

--- dwm/src/dwm-6.0/config.h
+++ dwm/src/dwm-6.0/config.h
@@ -30,7 +30,7 @@ static const Bool resizehints = True; /* True means respect size hints in tiled 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
